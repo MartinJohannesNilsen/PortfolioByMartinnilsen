75707ac3cd0f109650a6f90d33d6425f6d24811c
Martin Johannes Nilsen <martin@ivar-nilsen.no>
2020-02-12 10:39:18 +0100

    Added button and refactor of publicfolder

--- src/Components/AboutCard/AboutCard.js
+++ src/Components/AboutCard/AboutCard.js
@@ -11,6 +11,7 @@ class AboutCard extends Component{
                     <h1>Martin Johannes Nilsen</h1>
                     <p>Jeg er en 20 år gammel gutt i fra Rygge, Østfold. Startet på dataingeniørstudiet  ved NTNU i Trondheim rett etter vgs, for å oppsøke en utdanning innen IT.  Jeg er en motivert person som jobber godt både selvstendig og i team. Ser på meg selv som en utdadvent person, og er glad i å møte nye mennesker og bygge nettverk.</p>
                     <a href={process.env.PUBLIC_URL + '/media/Martin_Johannes_Nilsen_CV.pdf'} target="_blank"><button type="button" class="btn btn-outline-light btn-lg"> CV </button></a>
+                    <a href="https://github.com/Martinnilsen99" target="_blank"><button type="button" class="btn btn-outline-light btn-lg"> GitHub </button></a>
                 </div>
                 <div>
                     <img id="AboutCardImage" src={portrait} alt="Portrait of Martin Johannes Nilsen" />
