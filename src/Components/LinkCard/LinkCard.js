8ff4bcae704daf774ca3c1870bc4643de51f9aa2
Martin.Johannes.Nilsen <martin.johannes.nilsen@ciber.no>
2020-08-03 02:31:48 +0200

    Gjort om til funksjonelle komponenter og lagt inn db på prosjekter

--- src/Components/LinkCard/LinkCard.js
+++ src/Components/LinkCard/LinkCard.js
@@ -1,45 +1,65 @@
-
-import React, {Component} from "react";
+import React from "react";
 import "../../CSS/LinkCard.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
-class LinkCard extends Component{
-    render(){
-        return(
-            <div id="LinkCardContainer">
-                <div class="LinkCardBox" id="LinkCardBox1">
-                    <a href="https://github.com/Martinnilsen99" target="_blank" rel="noopener noreferrer" tabindex="0" id="LinkCardBoxATag">
-                    <div>
-                        <FontAwesomeIcon icon={faGithub} class="LinkCardIcon" alt="Martin Johannes Nilsen's GitHub"/>
-                    </div>
-                    </a>
-                </div>
-                <div class="LinkCardBox" id="LinkCardBox2">
-                    <a href="https://www.linkedin.com/in/martinnilsen99/" target="_blank" rel="noopener noreferrer" tabindex="0" id="LinkCardBoxATag">
-                    <div>
-                        <FontAwesomeIcon icon={faLinkedin} class="LinkCardIcon" alt="Martin Johannes Nilsen's linkedIn"/>
-                    </div>
-                    </a>
-                </div>
-                <div class="LinkCardBox" id="LinkCardBox3">
-                    <a href="/media/Martin_Johannes_Nilsen_CV.pdf" target="_blank" tabindex="0" id="LinkCardBoxATag">
-                    <div>
-                        <img src={require("../../IMG/Icons/CV_Icon.png")} class="LinkCardIcon" id="LinkCardIcon3" alt="Martin Johannes Nilsen's CV"/>
-                    </div>
-                    </a>
-                </div>
-            </div>
-        )
-    }
-    
-}
+const LinkCard = (props) => {
+	return (
+		<div id="LinkCardContainer">
+			<div class="LinkCardBox" id="LinkCardBox1">
+				<a
+					href="https://github.com/Martinnilsen99"
+					target="_blank"
+					rel="noopener noreferrer"
+					tabIndex="0"
+					id="LinkCardBoxATag"
+				>
+					<div>
+						<FontAwesomeIcon
+							icon={faGithub}
+							class="LinkCardIcon"
+							id="LinkCardIcon1"
+							alt="Martin Johannes Nilsen's GitHub"
+						/>
+					</div>
+				</a>
+			</div>
+			<div className="LinkCardBox" id="LinkCardBox2">
+				<a
+					href="https://www.linkedin.com/in/martinnilsen99/"
+					target="_blank"
+					rel="noopener noreferrer"
+					tabIndex="0"
+					id="LinkCardBoxATag"
+				>
+					<div>
+						<FontAwesomeIcon
+							icon={faLinkedin}
+							class="LinkCardIcon"
+							alt="Martin Johannes Nilsen's linkedIn"
+						/>
+					</div>
+				</a>
+			</div>
+			<div className="LinkCardBox" id="LinkCardBox3">
+				<a
+					href="/media/Martin_Johannes_Nilsen_CV.pdf"
+					target="_blank"
+					tabIndex="0"
+					id="LinkCardBoxATag"
+				>
+					<div>
+						<img
+							src={require("../../IMG/Icons/CV_Icon.png")}
+							class="LinkCardIcon"
+							id="LinkCardIcon3"
+							alt="Martin Johannes Nilsen's CV"
+						/>
+					</div>
+				</a>
+			</div>
+		</div>
+	);
+};
 export default LinkCard;
-
-
-/*
-<div>
-    <FontAwesomeIcon icon={faPortrait} class="LinkCardIcon" id="LinkCardIcon3" alt="Martin Johannes Nilsen's CV"/>
-</div>
-*/
