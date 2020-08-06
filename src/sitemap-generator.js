dff11dcab5fa95f9ea1d988868a7689831249dd6
Martin.Johannes.Nilsen <martin.johannes.nilsen@ciber.no>
2020-08-07 00:38:50 +0200

    dynamic sitemapbuilder

--- src/sitemap-generator.js
+++ src/sitemap-generator.js
@@ -8,7 +8,7 @@ const Sitemap = require("react-router-sitemap").default;
 function generateSitemap() {
 	return new Sitemap(router)
 		.build("https://martinnilsen.no")
-		.save("../sitemap.xml");
+		.save("./public/sitemap.xml");
 }
 
 generateSitemap();
