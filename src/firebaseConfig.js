7de2b399c003f4b2859d1b55be7675c337b71452
MartinNilsen99 <martinjnilsen@icloud.com>
2020-12-20 02:28:38 +0100

    Fikset firebaseprosjekt

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,13 @@
 import firebase from "firebase/app";
 import "firebase/database";
 const config = {
-	apiKey: "AIzaSyCSodQob_FW3Txc9NLW0BTNVQR8wez9e40",
-	authDomain: "PortfolioByMartinnilsen.firebaseapp.com",
-	databaseURL: "https://martinnilsen-58652.firebaseio.com",
-	projectId: "martinnilsen-58652",
-	storageBucket: "martinnilsen-58652.appspot.com",
+	apiKey: "AIzaSyC4cdiMG-Zl9Ziq0HnkuhDYvnan1nP6Uxw",
+	authDomain: "portfoliobymartinnilsen.firebaseapp.com",
+	databaseURL: "https://portfoliobymartinnilsen-default-rtdb.europe-west1.firebasedatabase.app",
+	projectId: "portfoliobymartinnilsen",
+	storageBucket: "portfoliobymartinnilsen.appspot.com",
+	messagingSenderId: "365385732369",
+	appId: "1:365385732369:web:40d9e17460bd1c682c7cb0"
 };
 let firebaseConfig = firebase.initializeApp(config);
 export default firebaseConfig;
