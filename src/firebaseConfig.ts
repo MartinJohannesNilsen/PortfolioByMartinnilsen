28687f8757cc44d1c0f21350f2d09e7cd75211ef
Martin Johannes Nilsen <martinjnilsen@icloud.com>
2022-05-28 12:44:57 +0200

    firebaseConfig

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import firebase from "firebase/app";
+import "firebase/database";
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  databaseURL: process.env.REACT_APP_FIREBASE_databaseURL,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+};
+let firebaseConfig = firebase.initializeApp(config);
+export default firebaseConfig;
